f2450bc8717016ee36e965bc83c18b99510a6f28
Pavel Bulygin <103452237+devbulygin@users.noreply.github.com>
2025-04-07 22:53:19 +0400

    Update translate.js

--- translate.js
+++ translate.js
@@ -8,7 +8,6 @@
     };
 
     function TranslateInit() {
-        // Получаем текущий язык из cookies
         let code = TranslateGetCode();
 
         // Находим активный флаг
@@ -26,10 +25,10 @@
         TranslateEventHandler('click', '[data-google-lang]', function (el) {
             const targetLang = el.getAttribute("data-google-lang");
 
-            // Если текущий язык уже выбран, не перезагружаем страницу
+            // Если текущий язык уже выбран, не делаем ничего
             if (targetLang === code) return;
 
-            // Записываем куку для выбранного языка
+            // Обновляем куку с новым языком
             const newCookieVal = "/" + googleTranslateConfig.lang + "/" + targetLang;
             TranslateCookieHandler(newCookieVal, googleTranslateConfig.domain);
 
@@ -39,14 +38,14 @@
     }
 
     function TranslateGetCode() {
-        // Получаем язык из куки или дефолтный
+        // Если куки нет, то передаем дефолтный язык
         const cookie = Cookies.get('googtrans') || "/";
         const match = cookie.match(/(?!^\/)[^\/]*$/gm);
         return match ? match[0] : googleTranslateConfig.lang;
     }
 
     function TranslateCookieHandler(val, domain) {
-        // Устанавливаем куки для выбранного языка
+        // Записываем куки для выбранного языка
         Cookies.set('googtrans', val);
         Cookies.set('googtrans', val, { domain: "." + document.domain });
 
